844876782bcab69216252936af4eefc07ff7d894
naveen.singh <naveen.singh@itelasoft.com>
2023-10-30 17:44:15 +0530

    swiper component

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { fetchMovieDetails } from '../../services/apiHandler';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/swiper-bundle.css';
 import './Card.css';
+import SwiperComponent from '../Swiper/Swiper';
 
 const Card = () => {
     const [popularMovies, setPopularMovies] = useState([]);
@@ -30,17 +26,7 @@ const Card = () => {
     return (
         <div className="card-container">
             <h2>Popular Movies</h2>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <Swiper spaceBetween={10} slidesPerView={5} navigation={true} modules={[Navigation]}>
-                    {popularMovies.map((movie) => (
-                        <SwiperSlide key={movie.id} className="movie-card">
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            )}
+            {loading ? <p>Loading...</p> : <SwiperComponent movies={popularMovies} />}
         </div>
     );
 };
