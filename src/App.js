c63f83644a47b677afa76ededea7e5872f6ce57d
naveen.singh <naveen.singh@itelasoft.com>
2023-10-31 13:52:44 +0530

    updated slider per view slide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
 function App() {
     return (
         <div className="App">
